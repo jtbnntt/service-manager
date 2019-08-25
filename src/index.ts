7268668286cd4941860bc1ba25b6ec6b43dc030d
jtbnntt <10692730+jtbnntt@users.noreply.github.com>
2019-08-24 17:25:42 -0700

    Export Service interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { default as Service } from './service';
 export { default as ServiceBuilder } from './service-builder';
 export { default as ServiceManager } from './service-manager';
 export { default as ServiceRegistry } from './service-registry';
